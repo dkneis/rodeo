4bcf781aae61d91ea7d7a95187bfd6b8c6e78067
dkneis <david.kneis@tu-dresden.de>
2014-11-14 10:45:43 +0000

    ENH: Language of generated code can be selected now (R or Fortran)

--- R/methods_generate.r
+++ R/methods_generate.r
@@ -13,24 +13,66 @@
 # state variable (with all derivatives being zero).
 
 
-rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
+rodeo$methods( generate = function(name="derivs", size=1, lang="R") {
     "Generate code to compute the variables' derivatives with respect to time.
     \\bold{Arguments:} \\code{name}: A string giving the name for the generated
     function; \\code{size}: An integer (default 1) specifying the number of
     instances of each state variable, e.g. in spatially distributed models.
-    \\code{return_proc}: If TRUE, the generated function also returns the values of process rates.
+    \\code{lang}: The language of generated code (currently 'R' or 'F').
     \\bold{Returns:} The generated function as a character string.
     "
+  # Constants
+  newline= ifelse(.Platform$OS.type=="windows","\r\n","\n")
+  nameVecDrvs= "dydt"
+  nameVecProc= "proc"
 
-  # We work with local copies here!
+  # Set language-specific constants
+  lang= substr(tolower(lang),1,1)
+  if (lang == "r") {
+    L= list(
+      funOpen= paste0(name," = function(time, vars, pars) {"),
+      argDecl="",
+      funClose= "}",
+      vecOpen= "unname(c(",
+      vecClose="))",
+      eleOpen= "[",
+      eleClose="]",
+      lstOpen="list(",
+      lstClose=")",
+      lstAppend=",",
+      cont="",
+      com= "#"
+    )
+  } else if (lang == "f") {
+    L= list(
+      funOpen= paste0("subroutine ",name,"(time, vars, pars, ",nameVecDrvs,", ",nameVecProc,")"),
+      funClose= "end subroutine",
+      argDecl=paste0(ifelse(length(funs) > 0,paste0("  use functions",newline),""),
+                     "  implicit none",newline,
+                     "  double precision, intent(in):: time",newline,
+                     "  double precision, dimension(",length(vars)*size,"), intent(in):: vars",newline,
+                     "  double precision, dimension(",length(pars),"), intent(in):: pars",newline,
+                     "  double precision, dimension(",length(vars)*size,"), intent(out):: ",nameVecDrvs,newline,
+                     "  double precision, dimension(",length(proc)*size,"), intent(out):: ",nameVecProc,newline),
+      vecOpen= "(/",
+      vecClose="/)",
+      eleOpen= "(",
+      eleClose=")",
+      lstOpen="",
+      lstClose="",
+      lstAppend="",
+      cont=" &",
+      com= "!"
+    )
+  } else {
+    stop("requested language not supported")
+  }
+
+  # We work with local copies (since this is a reference class!)
   AUXX= auxx
   PROC= proc
   STOX= stox
 
-  # Constants
-  ## Type of parenthesis used for array subscripts
-  rangeOpen="["
-  rangeClose="]"
   # Regexp patterns that preceed/follow valid identifier names. These patterns
   # are important to avoid partial matches, e.g. to properly distinguish between
   # two identifiers "temp" and "temperature". 
@@ -69,7 +111,7 @@ rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
   # Here, the index is set for the case of a 0D-model - it is reset later
   for (i in 1:ncol(STOX)) {
     patt= paste0(beforeName,names(STOX)[i],afterName)
-    repl= paste0("\\1","vars",rangeOpen,i,rangeClose,"\\2")
+    repl= paste0("\\1","vars",L$eleOpen,i,L$eleClose,"\\2")
     PROC= gsub(pattern=patt, replacement=repl, x=PROC)
     for (n in 1:ncol(STOX)) {
       STOX[,n]= gsub(pattern=patt, replacement=repl, x=STOX[,n])
@@ -80,7 +122,7 @@ rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
   # Here, the index is set for the case of a 0D-model - it is reset later
   for (i in 1:length(pars)) {
     patt= paste0(beforeName,names(pars)[i],afterName)
-    repl= paste0("\\1","pars",rangeOpen,i,rangeClose,"\\2")
+    repl= paste0("\\1","pars",L$eleOpen,i,L$eleClose,"\\2")
     PROC= gsub(pattern=patt, replacement=repl, x=PROC)
     for (n in 1:ncol(STOX)) {
       STOX[,n]= gsub(pattern=patt, replacement=repl, x=STOX[,n])
@@ -88,27 +130,24 @@ rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
   }  
 
   # Create code
-  newline= ifelse(.Platform$OS.type=="windows","\r\n","\n")
-  code=paste0("# THIS IS A GENERATED FILE - EDITING DOESN'T MAKE SENSE",newline)
-  code=paste0(code,newline)
-  code=paste0(code,"# This function returns the state variables' derivatives with resp. to time.",newline)
-  code=paste0(code,"# Does it also return the values of process rates? : ",ifelse(return_proc,"Yes","No"),newline)
+  code=paste0(L$com," THIS IS A GENERATED FILE - EDITING DOESN'T MAKE SENSE",newline)
   code=paste0(code,newline)  
-  code=paste0(code,name," = function(time,vars,pars) {",newline)
-  code=paste0(code,"list(",newline) # return values is wrapped into a list
+  code=paste0(code,L$funOpen,newline)
+  code=paste0(code,L$argDecl,newline)
+  code=paste0(code,L$lstOpen,newline)
 
   ##############################################################################
   # Derivatives of state variables
   ##############################################################################
-  code=paste0(code,"  dytd= unname(c(",newline)
+  code=paste0(code,"  ",nameVecDrvs,"= ",L$vecOpen,L$cont,newline)
   for (n in 1:ncol(STOX)) {
     if (n > 1) {
-      code=paste0(code,"    ,",newline)
+      code=paste0(code,"    ,",L$cont,newline)
     }
-    code=paste0(code,"    # Variable '",names(STOX)[n],"' with ",size," level",ifelse(size>1,"s",""),newline)
+    code=paste0(code,"    ",L$com," Variable '",names(STOX)[n],"' with ",size," level",ifelse(size>1,"s",""),newline)
     for (iunit in 1:size) {
       if (iunit > 1) {
-        code=paste0(code,"      ,",newline)
+        code=paste0(code,"      ,",L$cont,newline)
       }
       # Update index of variables, accounting for the spatial unit
       # Note: A dummy character '@' is inserted on value replacement in order to
@@ -117,26 +156,26 @@ rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
       local_PROC= PROC
       local_STOX= STOX
       for (q in 1:ncol(STOX)) {
-        local_PROC= gsub(pattern=paste0("vars",rangeOpen,q,rangeClose),
-          replacement=paste0("vars@",rangeOpen,(q-1)*size+iunit,rangeClose),
+        local_PROC= gsub(pattern=paste0("vars",L$eleOpen,q,L$eleClose),
+          replacement=paste0("vars@",L$eleOpen,(q-1)*size+iunit,L$eleClose),
           x=local_PROC, fixed=TRUE)
         for (p in 1:ncol(STOX)) {
-          local_STOX[,p]= gsub(pattern=paste0("vars",rangeOpen,q,rangeClose),
-            replacement=paste0("vars@",rangeOpen,(q-1)*size+iunit,rangeClose),
+          local_STOX[,p]= gsub(pattern=paste0("vars",L$eleOpen,q,L$eleClose),
+            replacement=paste0("vars@",L$eleOpen,(q-1)*size+iunit,L$eleClose),
             x=local_STOX[,p], fixed=TRUE)
         }
       }
       # Remove dummy character
-      local_PROC= gsub(pattern=paste0("vars@",rangeOpen),replacement=paste0("vars",rangeOpen), x=local_PROC, fixed=TRUE)
+      local_PROC= gsub(pattern=paste0("vars@",L$eleOpen),replacement=paste0("vars",L$eleOpen), x=local_PROC, fixed=TRUE)
       for (p in 1:ncol(STOX)) {
-        local_STOX[,p]= gsub(pattern=paste0("vars@",rangeOpen),replacement=paste0("vars",rangeOpen), x=local_STOX[,p], fixed=TRUE)
+        local_STOX[,p]= gsub(pattern=paste0("vars@",L$eleOpen),replacement=paste0("vars",L$eleOpen), x=local_STOX[,p], fixed=TRUE)
       }
       # Assemble expressions
       buffer=""
       for (k in 1:length(local_PROC)) {
         if (grepl(pattern="[^0]", x=local_STOX[k,n])) { # Suppress terms where a stoichiometry factor is zero
           if (nchar(buffer) > 0) {
-            buffer= paste0(buffer," +",newline)
+            buffer= paste0(buffer," +",L$cont,newline)
           }
           buffer=paste0(buffer,"      (",local_PROC[k],") * (",local_STOX[k,n],")")
         }
@@ -144,44 +183,41 @@ rodeo$methods( generate = function(name="derivs", size=1, return_proc=TRUE) {
       if (nchar(buffer) == 0) {
         buffer= "      0"  # treat case where all stoichiometry factors are zero
       }
-      code= paste0(code,buffer,newline)
+      code= paste0(code,buffer,L$cont,newline)
 
     }
   }
-  code=paste0(code,"  ))",newline) # End of derivatives vector
+  code=paste0(code,"  ",L$vecClose,newline) # End of derivatives vector
 
   ##############################################################################
-  # Process rates (optional output)
+  # Process rates
   ##############################################################################
-  if (return_proc) {
-    code=paste0(code,"  ,proc= unname(c(",newline)
-    for (n in 1:length(PROC)) {
-      if (n > 1) {
-        code=paste0(code,"    ,",newline)
-      }
-      code=paste0(code,"    # Process rate '",names(proc)[n],"' with ",size," level",ifelse(size>1,"s",""),newline)
-      for (iunit in 1:size) {
-        if (iunit > 1) {
-          code=paste0(code,"      ,",newline)
-        }
-        # Update index of variables, accounting for the spatial unit
-        # Note: See comments for the same procedure in the section dealing with derivatives
-        local_PROC_n= PROC[n]
-        for (q in 1:ncol(STOX)) {
-          local_PROC_n= gsub(pattern=paste0("vars",rangeOpen,q,rangeClose),
-            replacement=paste0("vars@",rangeOpen,(q-1)*size+iunit,rangeClose),
-            x=local_PROC_n, fixed=TRUE)
-        }
-        local_PROC_n= gsub(pattern=paste0("vars@",rangeOpen),replacement=paste0("vars",rangeOpen), x=local_PROC_n, fixed=TRUE)
-        # Add to code
-        code= paste0(code,local_PROC_n,newline)
-      }
+  code=paste0(code,"  ",L$lstAppend,nameVecProc,"=",L$vecOpen,L$cont,newline)
+  for (n in 1:length(PROC)) {
+    if (n > 1) {
+      code=paste0(code,"    ,",L$cont,newline)
     }
-    code=paste0(code,"  ))",newline) # End of process rates vector
+    code=paste0(code,"    ",L$com," Process rate '",names(proc)[n],"' with ",size," level",ifelse(size>1,"s",""),newline)
+    for (iunit in 1:size) {
+      if (iunit > 1) {
+        code=paste0(code,"      ,",L$cont,newline)
+      }
+      # Update index of variables, accounting for the spatial unit
+      # Note: See comments for the same procedure in the section dealing with derivatives
+      local_PROC_n= PROC[n]
+      for (q in 1:ncol(STOX)) {
+        local_PROC_n= gsub(pattern=paste0("vars",L$eleOpen,q,L$eleClose),
+          replacement=paste0("vars@",L$eleOpen,(q-1)*size+iunit,L$eleClose),
+          x=local_PROC_n, fixed=TRUE)
+      }
+      local_PROC_n= gsub(pattern=paste0("vars@",L$eleOpen),replacement=paste0("vars",L$eleOpen), x=local_PROC_n, fixed=TRUE)
+      # Add to code
+      code= paste0(code,"      ",local_PROC_n,L$cont,newline)
+    }
   }
-
-  code=paste0(code,")",newline) # End of list
-  code=paste0(code,"}",newline)
+  code=paste0(code,"  ",L$vecClose,newline)
+  code=paste0(code,L$lstClose,newline)
+  code=paste0(code,L$funClose,newline)
   return(code)
 })
 
