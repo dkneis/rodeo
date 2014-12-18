41f4b2e029cf742cd03b2226220f34ba69c433d5
dkneis <david.kneis@tu-dresden.de>
2014-12-18 16:49:03 +0000

    ENH: More convenient set-methods, improved error messages

--- R/methods_set.r
+++ R/methods_set.r
@@ -1,17 +1,25 @@
 
 rodeo$methods( setVars= function(v) {
     "Initializes field \\code{vars} to the values in the named vector \\code{v}"
-   if (!identical(sort(names(v)), sort(names(.self$vars))))
-     stop("length/names of input vector not matching length/names of target vector")
-   vars[names(v)] <<- v
+   if (is.null(names(v)))
+     stop("input must be a named vector")
+   miss= names(.self$vars)[! (names(.self$vars) %in% names(v))]
+   if (length(miss) > 0)
+     stop(paste0("the following element",ifelse(length(miss)>1,"s are"," is"),
+       " missing in the input vector: '",paste(miss,collapse="', '"),"'"))
+   vars <<- v[names(vars)]
    return(invisible(NULL))
 })
 
 rodeo$methods( setPars= function(v) {
     "Initializes field \\code{pars} to the values in the named vector \\code{v}"
-   if (!identical(sort(names(v)), sort(names(.self$pars))))
-     stop("length/names of input vector not matching length/names of target vector")
-   pars[names(v)] <<- v
+   if (is.null(names(v)))
+     stop("input must be a named vector")
+   miss= names(.self$pars)[! (names(.self$pars) %in% names(v))]
+   if (length(miss) > 0)
+     stop(paste0("the following element",ifelse(length(miss)>1,"s are"," is"),
+       " missing in the input vector: '",paste(miss,collapse="', '"),"'"))
+   pars <<- v[names(pars)]
    return(invisible(NULL))
 })
 
