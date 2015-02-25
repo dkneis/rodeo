4bef021b98b1e6a9a6054e39b5c540604b5db277
dkneis <david.kneis@tu-dresden.de>
2015-02-25 08:41:19 +0000

    REV: Replaced set-methods by sortLike-methods (rodeo object no longer stored numeric values of states/parameters)

--- R/methods_sortLike.r
+++ R/methods_sortLike.r
@@ -1,25 +1,23 @@
 
-rodeo$methods( setVars= function(v) {
-    "Initializes field \\code{vars} to the values in the named vector \\code{v}"
+rodeo$methods( sortLikeVars= function(v) {
+    "Sorts a named vector \\code{v} according to the order of state variables"
    if (is.null(names(v)))
      stop("input must be a named vector")
-   miss= names(.self$vars)[! (names(.self$vars) %in% names(v))]
+   miss= .self$vars[! (.self$vars %in% names(v))]
    if (length(miss) > 0)
      stop(paste0("the following element",ifelse(length(miss)>1,"s are"," is"),
        " missing in the input vector: '",paste(miss,collapse="', '"),"'"))
-   vars <<- v[names(vars)]
-   return(invisible(NULL))
+   return(v[.self$vars])
 })
 
-rodeo$methods( setPars= function(v) {
-    "Initializes field \\code{pars} to the values in the named vector \\code{v}"
+rodeo$methods( sortLikePars= function(v) {
+    "Sorts a named vector \\code{v} according to the order of parameters"
    if (is.null(names(v)))
      stop("input must be a named vector")
-   miss= names(.self$pars)[! (names(.self$pars) %in% names(v))]
+   miss= .self$pars[! (.self$pars %in% names(v))]
    if (length(miss) > 0)
      stop(paste0("the following element",ifelse(length(miss)>1,"s are"," is"),
        " missing in the input vector: '",paste(miss,collapse="', '"),"'"))
-   pars <<- v[names(pars)]
-   return(invisible(NULL))
+   return(v[.self$pars])
 })
 
