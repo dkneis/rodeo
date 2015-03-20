7b6e10fa8929fbb86552603743ed801f005c5c20
dkneis <david.kneis@tu-dresden.de>
2015-03-20 13:49:48 +0000

    MIN: Improved error messages

--- R/methods_arrange.r
+++ R/methods_arrange.r
@@ -3,21 +3,23 @@
 arrangeGeneric= function(x, itemNames, warnUnused=TRUE) {
   # Check inputs
   if (!is.list(x))
-    stop("argument 'x' must be a list")
+    stop("actual argument must be a list")
+  if (length(x) == 0)
+    stop("input list has length zero")
   bad= itemNames[!(itemNames %in% names(x))]
   if (length(bad) > 0)
-    stop(paste0("list 'x' does not provide data for the following",
+    stop(paste0("input list does not provide data for the following",
       " item(s): '",paste(bad,collapse="', '"),"'"))
   if (warnUnused) {
     bad= names(x)[!(names(x) %in% itemNames)]
     if (length(bad) > 0)
-      warning(paste0("the following element(s) from list 'x' were ignored: '",
+      warning(paste0("the following element(s) from the input list were ignored: '",
         paste(bad,collapse="', '"),"'"))
   }
   # Transform into data frame to get the same number of levels for all variables
   sizes= unique(sapply(x, length))
   if ((length(sizes) > 2) || ((length(sizes) == 2) && (min(sizes) != 1)))
-    stop("elements of list 'x' must either be scalars or vectors of a common length")
+    stop("elements of input list must either be scalars or vectors of a common length")
   x= data.frame(x)
   # Sort/drop columns
   x= x[,itemNames]
@@ -25,7 +27,7 @@ arrangeGeneric= function(x, itemNames, warnUnused=TRUE) {
   x= unlist(x)
   # Check type
   if (any(!is.numeric(x)))
-    stop("non-numeric data detected in list 'x'")
+    stop("non-numeric data detected in input list")
   # Done
   return(x)
 }
