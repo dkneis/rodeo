9f67e0210113b7854310ea7e34680bc8c11c7b10
dkneis <david.kneis@tu-dresden.de>
2015-03-20 08:19:22 +0000

    FIX: Bad data access in method stoichiometryMatrix() fixed

--- R/methods_stoichiometryMatrix.r
+++ R/methods_stoichiometryMatrix.r
@@ -3,7 +3,7 @@ rodeo$methods( stoichiometryMatrix = function() {
   m= matrix("0", ncol=nrow(.self$VARS), nrow=nrow(.self$PROS))
   colnames(m)= .self$VARS$name
   rownames(m)= .self$PROS$name
-  for (i in 1:nrow(stoichiometry)) {
+  for (i in 1:nrow(.self$STOI)) {
     m[.self$STOI$process[i], .self$STOI$variable[i]]= .self$STOI$expression[i]
   }
   return(m)
