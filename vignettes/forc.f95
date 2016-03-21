ba15a121b66d1150f8ac1b96c1af8782f3b4fe05
dkneis <david.kneis@tu-dresden.de>
2016-03-21 10:19:53 +0000

    MIN: Added section to vignette

--- vignettes/forc.f95
+++ vignettes/forc.f95
@@ -1,21 +1,36 @@
 ! THIS IS A GENERATED FILE 
 
-    include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsGenericMethods.f95'
-    module forcings
-    use forcings_generic
-    implicit none
-    private TSeries, readTS, interpol
-    contains
-      function temp (time) result (res)
-        character(len=256), parameter:: file='meteo.txt'
-        character(len=256), parameter:: col='temp'
-        integer, parameter:: lweight= -1
-        include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsInclude.f95'
-      end function
-      function humid (time) result (res)
-        character(len=256), parameter:: file='meteo.txt'
-        character(len=256), parameter:: col='humid'
-        integer, parameter:: lweight= -1
-        include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsInclude.f95'
-      end function
-    end module
+include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsGenericMethods.f95'
+module forcings
+use forcings_generic
+implicit none
+private TSeries, readTS, interpol
+contains
+
+  function temp (time) result (res)
+  double precision, intent(in):: time
+  character(len=256), parameter:: file='meteo.txt'
+  character(len=256), parameter:: col='temp'
+  integer, parameter:: lweight= -1
+  logical, save:: firstCall= .TRUE.
+  integer, save:: latest= 1
+  type(TSeries), save:: x
+  double precision, parameter:: NA= huge(0d0)
+  character(len=512):: errmsg
+  double precision:: res
+  include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsInclude.f95'
+end function
+  function humid (time) result (res)
+  double precision, intent(in):: time
+  character(len=256), parameter:: file='meteo.txt'
+  character(len=256), parameter:: col='humid'
+  integer, parameter:: lweight= -1
+  logical, save:: firstCall= .TRUE.
+  integer, save:: latest= 1
+  type(TSeries), save:: x
+  double precision, parameter:: NA= huge(0d0)
+  character(len=512):: errmsg
+  double precision:: res
+  include '/home/dkneis/R/x86_64-pc-linux-gnu-library/3.2/rodeo/fortran/forcingsInclude.f95'
+end function
+end module
